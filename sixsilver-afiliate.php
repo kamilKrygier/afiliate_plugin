82825bdd6eb1feaa50b470356a2c96493786ef77
Kamil Krygier <32956194+kamilKrygier@users.noreply.github.com>
2023-09-29 09:40:54 +0200

    Fixed $sale_price undefined variable

--- sixsilver-afiliate.php
+++ sixsilver-afiliate.php
@@ -149,82 +149,97 @@ function call_woocommerce_api($stock_status, $category, $orderby, $order, $attri
 // Register shortcode
 function register_products_shortcode($atts) {
 
-    // Default values for shortcode
-    $atts = shortcode_atts(array(
-        'stock_status' => 'instock',
-        'category' => '143',
-        'columns' => 4,
-        'orderby' => 'popularity',
-        'order' => 'desc',
-        'attribute' => '',
-        'attribute_term' => '',
-        'per_page' => 8,
-        'min_price' => '',
-        'max_price' => '',
-        'sku' => '',
-        'include' => '',
-        'exclude' => '',
+    if(!is_admin()){
 
-    ), $atts, 'SIXSILVER_PRODUCTS');
-    
-    // API call
-    $products = call_woocommerce_api(   $atts['stock_status'], 
-                                        $atts['category'], 
-                                        $atts['orderby'], 
-                                        $atts['order'], 
-                                        $atts['attribute'], 
-                                        $atts['attribute_term'], 
-                                        $atts['per_page'], 
-                                        $atts['min_price'], 
-                                        $atts['max_price'], 
-                                        $atts['sku'], 
-                                        $atts['include'], 
-                                        $atts['exclude'],
-                                        'publish');
-    
-    // Check if API call returned any data
-    if(empty($products)) {
-        return "<p style='text-align: center;'>" . __('No results found.', 'default') . "</p>";
-    }
+        // Default values for shortcode
+        $atts = shortcode_atts(array(
+            'stock_status' => 'instock',
+            'category' => '143',
+            'columns' => 4,
+            'orderby' => 'popularity',
+            'order' => 'desc',
+            'attribute' => '',
+            'attribute_term' => '',
+            'per_page' => 8,
+            'min_price' => '',
+            'max_price' => '',
+            'sku' => '',
+            'include' => '',
+            'exclude' => '',
 
-    // Building HTML output
-    $output = '<ul class="six_products_wrapper">';
-    foreach ($products as $product) {
-
-        // Set prices variables
-        if($product['type'] == 'variable'){
-            $regular_price = $product['baselinker_variations'][0]['regular_price'];
-            if($product['baselinker_variations'][0]['sale_price'] != '') $sale_price = $product['baselinker_variations'][0]['sale_price'];
-        } else if($product['type'] == 'simple'){
-            if($product['regular_price'] != '') $regular_price = $product['regular_price'];
-            else $regular_price = $product['price'];
-            if($product['sale_price'] != '') $sale_price = $product['sale_price'];
+        ), $atts, 'SIXSILVER_PRODUCTS');
+        
+        // API call
+        $products = call_woocommerce_api(   $atts['stock_status'], 
+                                            $atts['category'], 
+                                            $atts['orderby'], 
+                                            $atts['order'], 
+                                            $atts['attribute'], 
+                                            $atts['attribute_term'], 
+                                            $atts['per_page'], 
+                                            $atts['min_price'], 
+                                            $atts['max_price'], 
+                                            $atts['sku'], 
+                                            $atts['include'], 
+                                            $atts['exclude'],
+                                            'publish');
+        
+        // Check if API call returned any data
+        if(empty($products)) {
+            return "<p style='text-align: center;'>" . __('No results found.', 'default') . "</p>";
         }
 
+        // Building HTML output
+        $output = '<ul class="six_products_wrapper">';
+        foreach ($products as $product) {
+            
+            $sale_price = '';
 
-        // Build the product
-        $output .= '<li class="six_product">';
-        $output .= '<a href="' . $product['permalink'] . '">';
-        $output .= '<div class="six_image_wrapper"><img src="' . $product['images'][0]['src'] . '" alt="' . $product['name'] . '">';
+            // Set prices variables
+            if($product['type'] == 'variable'){
+                $regular_price = $product['baselinker_variations'][0]['regular_price'];
+                if(isset($product['baselinker_variations'][0]['sale_price']) && $product['baselinker_variations'][0]['sale_price'] !== "") $sale_price = $product['baselinker_variations'][0]['sale_price'];
+            } else if($product['type'] == 'simple'){
+                if(isset($product['regular_price']) && $product['regular_price'] !== "") $regular_price = $product['regular_price'];
+                    else $regular_price = $product['price'];
+                if(isset($product['sale_price']) && $product['sale_price'] !== "") $sale_price = $product['sale_price'];
+            }
 
-        if($sale_price) $output .= '<div class="six_sale_badge"><span>SALE!</span></div>';
-        else if($regular_price >= 200) $output .= '<div class="six_sale_badge"><span>DARMOWA DOSTAWA</span></div>';
+            $parsed_url = parse_url(get_site_url());
+            $page_type = "";
+            if(is_singular('post')) $page_type = "POST";
+            else if(is_singular('page')) $page_type = "PAGE";
+            else if(is_singular('product')) $page_type = "PRODUCT"; 
+            else if(is_singular('attachment')) $page_type = "ATTACHMENT"; 
+            else if(is_singular('revision')) $page_type = "REVISION"; 
+            else if(is_singular('menu')) $page_type = "ATTACHMENT"; 
+            else $page_type = "OTHER";
 
-        $output .= '</div>';
-        $output .= '<p>' . $product['name'] . '</p>';
-        if($sale_price) $output .= '<p class="six_price"><del>' . $regular_price . 'zł</del> <ins>' . $sale_price . 'zł</ins></p>';
-        else $output .= '<p class="six_price"><ins>' . $regular_price . 'zł</ins></p>';
-        $output .= '</a>';
-        $output .= '</li>';
+            // Build the product
+            $output .= '<li class="six_product">';
+            $output .= '<a href="' . $product['permalink'] . '?utm_source='. sanitize_title($parsed_url['host']) .'&utm_medium='. $page_type .'">';
+            $output .= '<div class="six_image_wrapper"><img src="' . $product['images'][0]['src'] . '" alt="' . $product['name'] . '">';
 
+            if($sale_price) $output .= '<div class="six_sale_badge"><span>SALE!</span></div>';
+            else if($regular_price >= 200) $output .= '<div class="six_sale_badge"><span>DARMOWA DOSTAWA</span></div>';
+
+            $output .= '</div>';
+            $output .= '<p>' . $product['name'] . '</p>';
+            if($sale_price) $output .= '<p class="six_price"><del>' . $regular_price . 'zł</del> <ins>' . $sale_price . 'zł</ins></p>';
+            else $output .= '<p class="six_price"><ins>' . $regular_price . 'zł</ins></p>';
+            $output .= '</a>';
+            $output .= '</li>';
+
+        }
+        $output .= '</ul>';
+
+        $columns_1140 = 3;
+        if($atts['columns'] > 4) $columns_1140 = ceil($atts['columns'] / 2);
+
+        $output .= '<style> .six_products_wrapper{display: grid;grid-template-columns: repeat('.$atts['columns'].', 1fr);column-gap: .5em;row-gap: .5em;grid-auto-rows: max-content;list-style: none;width: 100%;overflow: hidden;padding: 0;max-width: 1440px !important;}.six_products_wrapper > .six_product{width: 100%;}.six_products_wrapper > .six_product > a{display: block;padding: 15px;touch-action: manipulation;cursor: pointer;text-decoration: initial;color: #090909;transition: ease-in-out .2s;}.six_products_wrapper > .six_product > a:hover{color: #262626;transition: ease-in-out .2s;}.six_products_wrapper > .six_product > a .six_image_wrapper{background: #f6f6f6;background: -webkit-radial-gradient(circle, #f6f6f6 0%, #f1f1f4 50%, #e7e7e9 100%);background: radial-gradient(circle, #f6f6f6 0%, #f1f1f4 50%, #e7e7e9 100%);margin-bottom: 15px;aspect-ratio: 3/4;object-fit: cover;display: flex;align-items: center;justify-content: center;margin-bottom: 15px;position: relative;}.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge{position: absolute;bottom: 0;left: 0;right: 0;width: 100%;display: flex;align-items: center;background-color: #BFA4DB;text-align: center;}.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge > span{color: #FFFFFF;text-transform: uppercase;margin: 0 auto;font-size: 15px;padding: 0 3px;}.six_products_wrapper > .six_product > a p{line-height: 1.1em;text-align: left;font-size: 15px;color: #090909;letter-spacing: .02em;margin-top: 0;margin-bottom: 10px;}.six_products_wrapper > .six_product img{max-width: 100%;height: auto;}.six_products_wrapper > .six_product .six_price{text-align: left;color: #888888;font-size: 14px;line-height: 18px;}@media screen and (max-width: 1140px){.six_products_wrapper{grid-template-columns: repeat('.$columns_1140 .', 1fr);}}@media screen and (max-width: 767px){.six_products_wrapper{grid-template-columns: repeat(2, 1fr);}}@media screen and (max-width: 445px){.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge > span{font-size: 11px;}}</style>';
+
+        return $output;
     }
-    $output .= '</ul>';
-
-    $columns_1140 = 3;
-    if($atts['columns'] > 4) $columns_1140 = ceil($atts['columns'] / 2);
-
-    $output .= '<style> .six_products_wrapper{display: grid;grid-template-columns: repeat('.$atts['columns'].', 1fr);column-gap: .5em;row-gap: .5em;grid-auto-rows: max-content;list-style: none;width: 100%;overflow: hidden;padding: 0;max-width: 1440px !important;}.six_products_wrapper > .six_product{width: 100%;}.six_products_wrapper > .six_product > a{display: block;padding: 15px;touch-action: manipulation;cursor: pointer;text-decoration: initial;color: #090909;transition: ease-in-out .2s;}.six_products_wrapper > .six_product > a:hover{color: #262626;transition: ease-in-out .2s;}.six_products_wrapper > .six_product > a .six_image_wrapper{background: #f6f6f6;background: -webkit-radial-gradient(circle, #f6f6f6 0%, #f1f1f4 50%, #e7e7e9 100%);background: radial-gradient(circle, #f6f6f6 0%, #f1f1f4 50%, #e7e7e9 100%);margin-bottom: 15px;aspect-ratio: 3/4;object-fit: cover;display: flex;align-items: center;justify-content: center;margin-bottom: 15px;position: relative;}.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge{position: absolute;bottom: 0;left: 0;right: 0;width: 100%;display: flex;align-items: center;background-color: #BFA4DB;text-align: center;}.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge > span{color: #FFFFFF;text-transform: uppercase;margin: 0 auto;font-size: 15px;padding: 0 3px;}.six_products_wrapper > .six_product > a p{line-height: 1.1em;text-align: left;font-size: 15px;color: #090909;letter-spacing: .02em;margin-top: 0;margin-bottom: 10px;}.six_products_wrapper > .six_product img{max-width: 100%;height: auto;}.six_products_wrapper > .six_product .six_price{text-align: left;color: #888888;font-size: 14px;line-height: 18px;}@media screen and (max-width: 1140px){.six_products_wrapper{grid-template-columns: repeat('.$columns_1140 .', 1fr);}}@media screen and (max-width: 767px){.six_products_wrapper{grid-template-columns: repeat(2, 1fr);}}@media screen and (max-width: 445px){.six_products_wrapper > .six_product > a .six_image_wrapper .six_sale_badge > span{font-size: 11px;}}</style>';
-
-    return $output;
+    return;
 }
 add_shortcode('SIXSILVER_PRODUCTS', 'register_products_shortcode');
